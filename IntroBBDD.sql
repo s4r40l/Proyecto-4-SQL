e870592e5b5980a6b2297ff55a57e10d141754cc
Sara Oliva <s4r40l1v4@gmail.com>
2026-09-16 12:37:57 +0200

    Add multiple SQL exercises to IntroBBDD.sql

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -4,3 +4,20 @@ FROM flights
 WHERE status = 'On Time';
 
 -- EJERCICIO 2
+SELECT *
+FROM bookings
+WHERE total_amount > 1000000;
+
+-- EJERCICIO 3
+SELECT *
+FROM aircrafts_data;
+
+-- EJERCICIO 4
+SELECT flight_id
+FROM flights
+WHERE aircraft_code = '733';
+
+-- EJERCICIO 5
+SELECT *
+FROM tickets
+WHERE passenger_name LIKE '%IRINA%';
